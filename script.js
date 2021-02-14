add497e81a6e32c0cd7288d646c310f847d69dfb
suraj.be <suraj.be@media.net>
2021-02-14 19:08:36 +0530

    implemented modal functionality

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 'use strict';
 
 // Modal window
+const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function (e) {
-  // e.preventDefault();
+  e.stopPropagation();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
+  body.classList.add('modal-active');
 };
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
+  body.classList.remove('modal-active');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
